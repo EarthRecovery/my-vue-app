fa1dfd51b5078b180b1e8fd6ee5b1142d0cf0c65
EarthRecovery <ssyyl35@nottingham.edu.cn>
2023-04-07 21:47:34 +0800

    Change 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,58 @@
 <template>
   <div class="hello">
-    <p>
-      zhhsdsb
-    </p>
+    <div id="background">
+      <div id="block">
+        <h1>{{ headline }}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      headline: "Klein Kaslana",
+      img1: require("../assets/img1.jpg"),
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#background {
+  background: url("../assets/bg.jpg") no-repeat;
+  background-color: rgba(255, 255, 255, 0.5);
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.hello{
-  font-size:100;
+
+#block {
+  background: #ffffff;
+  background-color: rgba(255, 255, 255, 0.9);
+  height: 100%;
+  width: 75%;
+  position: absolute;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
+
+h1 {
+  font-size: 100px;
+  font-family: "Times New Roman", Times, serif;
+  color: #000000;
+  text-align: center;
+  margin: 0;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
